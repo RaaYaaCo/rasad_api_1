7818927b2815ce7af8c6f4086078f5c2ae5c382a
Shinigami1302 <hosseinfatehi1302@gmail.com>
2023-10-10 20:17:00 +0330

    Modify product model

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext as _
 
 
 class ProductType(models.Model):
-    pt_title = models.CharField(max_length='100', verbose_name=_('title'), unique=True, db_index=True)
+    pt_title = models.CharField(max_length=100, verbose_name=_('title'), unique=True, db_index=True)
 
     def __str__(self):
         return self.pt_title
@@ -14,7 +14,7 @@ class ProductType(models.Model):
 
 
 class Degree(models.Model):
-    d_title = models.CharField(max_length='100', verbose_name=_('title'), unique=True, db_index=True)
+    d_title = models.CharField(max_length=100, verbose_name=_('title'), unique=True, db_index=True)
 
     def __str__(self):
         return self.d_title
@@ -25,7 +25,7 @@ class Degree(models.Model):
 
 
 class Unit(models.Model):
-    un_title = models.CharField(max_length='100', verbose_name=_('title'), unique=True, db_index=True)
+    un_title = models.CharField(max_length=100, verbose_name=_('title'), unique=True, db_index=True)
 
     def __str__(self):
         return self.un_title
@@ -36,7 +36,7 @@ class Unit(models.Model):
 
 
 class Product(models.Model):
-    p_name = models.CharField(max_length='100', db_index=True, verbose_name=_('name'))
+    p_name = models.CharField(max_length=100, db_index=True, verbose_name=_('name'))
     P_description = models.TextField(verbose_name=_('description'), blank=True, null=True),
     image = models.ImageField(upload_to='/product/images/', verbose_name='image', blank=True, null=True)
     pt_id = models.ForeignKey(ProductType, on_delete=models.PROTECT, verbose_name=_('product type'), db_index=True)
