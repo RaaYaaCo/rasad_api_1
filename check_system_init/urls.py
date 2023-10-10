44a672c8241b48c092fdd275e849a73ccceb6b93
Shinigami1302 <hosseinfatehi1302@gmail.com>
2023-10-10 16:06:33 +0330

    Start check system

--- check_system_init/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-app_name = 'check_system_init'
-urlpatterns = [
-    path('anonymous/', views.RefreshTokenAnonymous.as_view(), name='token_anonymous')
-]
