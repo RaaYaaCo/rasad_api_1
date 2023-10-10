21be8bab4ac9dd61ca9f7e414f58de0579a4bdca
Shinigami1302 <hosseinfatehi1302@gmail.com>
2023-10-10 18:19:02 +0330

    Add product admin

--- product/admin.py
+++ product/admin.py
@@ -4,5 +4,26 @@ from .models import *
 # Register your models here.
 
 
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    pass
 
 
+@admin.register(Degree)
+class DegreeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductPrice)
+class ProductPriceAdmin(admin.ModelAdmin):
+    pass
